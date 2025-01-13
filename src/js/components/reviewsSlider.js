12850bb5f1dcd0d5268a8b6794f4641a1d286b5b
dmitrykuzan <dmitrykuzan@gmail.com>
2025-01-13 13:34:55 +0200

    swiper fix

--- src/js/components/reviewsSlider.js
+++ src/js/components/reviewsSlider.js
@@ -1,24 +1,23 @@
 export const reviewsSlider = () => {
   let reviewsSlider = new Swiper(".reviews-swiper", {
-    spaceBetween: 20,
     slidesPerView: 1,
+    spaceBetween: 20,
     centeredSlides: true,
     loop: true,
     pagination: {
       el: ".reviews__swiper-pagination",
       clickable: true,
     },
-    // autoplay: {
-    //   delay: 5000,
-    //   disableOnInteraction: false,
-    // },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
     breakpoints: {
       1020: {
         slidesPerView: 1.7,
         spaceBetween: 0,
       },
       1280: {
-        // spaceBetween: 20,
         slidesPerView: 3.5,
         spaceBetween: 20,
         autoHeight: true,
